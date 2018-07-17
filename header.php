4fd514d6b9560cfd3cab749286025f56f969d7d1
TAG <charliematina@gmail.com>
2018-07-17 15:49:00 +1200

    Added Google Analytics gtag to head tag.

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
-
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
@@ -28,6 +27,16 @@
             tests: {}
         });
         </script>
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-68230113-24"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-68230113-24');
+		</script>
 
 	</head>
 	<body <?php body_class(); ?>>
